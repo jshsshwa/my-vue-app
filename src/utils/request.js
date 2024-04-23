8f2f3374ba54fce78766706c7a2cf8b82d1e4035
Sean <jshsshwa@gmail.com>
2024-04-23 19:56:50 +0800

    增加註解

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     //设置项目基准地址
     baseURL: '/api',
     //设置请求超时时间为5秒钟
-    //timeout:5000
+    timeout:5000
 })
 //设置请求拦截器
 instance.interceptors.request.use(config => {
@@ -16,6 +16,7 @@ instance.interceptors.request.use(config => {
     return config
 })
 //设置响应拦截器
+//呼叫成功後觸發的事件
 instance.interceptors.response.use(response => {
     return response.data
 }, err => {
