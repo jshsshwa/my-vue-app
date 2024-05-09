a850f42e4105cdff1bad62e86de674138393ab4a
Sean <jshsshwa@gmail.com>
2024-05-09 21:12:23 +0800

    簡轉繁

--- src/api/pics.js
+++ src/api/pics.js
@@ -1,73 +1,73 @@
-//图库API
-//导入axios
-import request from '@/utils/request'
+//圖庫API
+//導入axios
+import request from '@/utils/request';
 
-//获取图库分类
-export const getPicsCate=(page,limit=10)=>{
+//獲取圖庫分類
+export const getPicsCate = (page, limit = 10) => {
     return request({
-        url:`admin/getPicsCateList/${page}`,
-        method:'GET',
-        params:{
+        url: `admin/getPicsCateList/${page}`,
+        method: 'GET',
+        params: {
             limit
         }
-    })
-}
+    });
+};
 
-//新增图库分类
-export const addPicsCateListFn=(data)=>{
+//新增圖庫分類
+export const addPicsCateListFn = (data) => {
     return request({
-        url:'admin/addPicsCateList',
-        method:'POST',
+        url: 'admin/addPicsCateList',
+        method: 'POST',
         data
-    })
-}
+    });
+};
 
-//修改图库分类
-export const editPiscCateList=(id,data)=>{
+//修改圖庫分類
+export const editPiscCateList = (id, data) => {
     return request({
-        url:`admin/editPicsList/${id}`,
-        method:'POST',
+        url: `admin/editPicsList/${id}`,
+        method: 'POST',
         data
-    })
-}
+    });
+};
 
-//删除图库分类
-export const delCateListFn=(id)=>{
+//刪除圖庫分類
+export const delCateListFn = (id) => {
     return request({
-        url:`admin/delPicsCateList/${id}/delete`,
-        method:'POST'
-    })
-}
+        url: `admin/delPicsCateList/${id}/delete`,
+        method: 'POST'
+    });
+};
 
-//根据分类id获取图库列表
-export const getPicsList=(id,page,limit)=>{
+//根據分類id獲取圖庫列表
+export const getPicsList = (id, page, limit) => {
     return request({
-        url:`admin/getPicList/${id}/image/${page}`,
-        params:{
+        url: `admin/getPicList/${id}/image/${page}`,
+        params: {
             limit
         }
-    })
-}
+    });
+};
 
-//图片重命名
-export const editPicNameFn=(id,name)=>{
+//圖片重命名
+export const editPicNameFn = (id, name) => {
     return request({
-        url:`admin/editPicName/${id}`,
-        method:'POST',
-        data:{
+        url: `admin/editPicName/${id}`,
+        method: 'POST',
+        data: {
             name
         }
-    })
-}
-//删除图片
-export const delPicFn=(ids)=>{
+    });
+};
+//刪除圖片
+export const delPicFn = (ids) => {
     return request({
-        url:'admin/delPic/delete_all',
-        method:'POST',
-        data:{
+        url: 'admin/delPic/delete_all',
+        method: 'POST',
+        data: {
             ids
         }
-    })
-}
+    });
+};
 
-export const uploadPicAction='/api/admin/image/upload'
+export const uploadPicAction = '/api/admin/image/upload';
--- src/components/AsidePicCate.vue
+++ src/components/AsidePicCate.vue
@@ -1,106 +1,104 @@
-<!-- 图库分类 -->
+<!-- 圖庫分類 -->
 <template>
     <el-aside width="200px">
         <div class="cateList">
-            <div class="cateItem" :class="{ active: item.id == isActive }" v-for="(item,i) in cateList" :key="i">
+            <div class="cateItem" :class="{active: item.id == isActive}" v-for="(item, i) in cateList" :key="i">
                 <span @click="changeCateList(item.id)">
                     {{ item.name }}
                 </span>
                 <em>
-                    <el-button type="primary" text @click="$emit('edit',item)">
+                    <el-button type="primary" text @click="$emit('edit', item)">
                         <el-icon>
                             <Edit />
                         </el-icon>
                     </el-button>
-                    <el-button type="primary" text @click="$emit('del',item)">
+                    <el-button type="primary" text @click="$emit('del', item)">
                         <el-icon>
                             <Close />
                         </el-icon>
                     </el-button>
                 </em>
             </div>
-
         </div>
         <div class="page">
-            <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :small="small"
-                :background="background" layout=" prev,  next" :total=total @size-change="handleSizeChange"
-                @current-change="handleCurrentChange" />
+            <el-pagination
+                v-model:current-page="currentPage"
+                v-model:page-size="pageSize"
+                :small="small"
+                :background="background"
+                layout=" prev,  next"
+                :total="total"
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+            />
         </div>
-        
     </el-aside>
-
-
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { getPicsCate } from '@/api/pics.js'
+import {ref} from 'vue';
+import {getPicsCate} from '@/api/pics.js';
 
-const cateList = ref([])
-//总条数
-const total = ref(0)
-//当前页
-const currentPage = ref(1)
-//每页几条数据
-const pageSize = ref(10)
-//默认选中状态
-const isActive = ref(0)
+const cateList = ref([]);
+//總條數
+const total = ref(0);
+//當前頁
+const currentPage = ref(1);
+//每頁幾條數據
+const pageSize = ref(10);
+//默認選中狀態
+const isActive = ref(0);
 
-
-const background = ref('background')
-const small = ref('small')
-
-
+const background = ref('background');
+const small = ref('small');
 
 const getPicsCateData = async () => {
-    const res = await getPicsCate(currentPage.value, pageSize.value)
-    console.log(res)
+    const res = await getPicsCate(currentPage.value, pageSize.value);
+    console.log(res);
     if (res.msg && res.msg !== 'ok') {
-        return
+        return;
     }
-    cateList.value = res.data.list
-    total.value = res.data.totalCount
-    //将第一条分类设置成默认选中状态
-    const item = cateList.value[0]
+    cateList.value = res.data.list;
+    total.value = res.data.totalCount;
+    //將第一條分類設置成默認選中狀態
+    const item = cateList.value[0];
     if (item) {
         //isActive.value = item.id
-        //保证一开始有数据 进来
-        changeCateList(item.id)
+        //保證一開始有數據 進來
+        changeCateList(item.id);
     }
-}
-getPicsCateData()
+};
+getPicsCateData();
 
-//选中图库分类id
+//選中圖庫分類id
 // const isActive = ref(0)
 
-
-//向父组件暴露方法
+//向父組件暴露方法
 defineExpose({
     getPicsCateData
-})
+});
 
-//修改显示条数
+//修改顯示條數
 const handleSizeChange = (val) => {
-    pageSize.value = val
-    getPicsCateData()
-}
-//修改分页页码
+    pageSize.value = val;
+    getPicsCateData();
+};
+//修改分頁頁碼
 const handleCurrentChange = (val) => {
-    currentPage.value = val
-    getPicsCateData()
-}
+    currentPage.value = val;
+    getPicsCateData();
+};
 
-//子组件向父组件传递数据
-const emit=defineEmits(["change"])
-//分类选择
-const changeCateList=(i)=>{
-    isActive.value=i
-    emit('change',i)
-}
-
+//子組件向父組件傳遞數據
+const emit = defineEmits(['change']);
+//分類選擇
+const changeCateList = (i) => {
+    isActive.value = i;
+    emit('change', i);
+};
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .el-aside {
     height: 100%;
     border-right: 1px solid #dbdbdb;
@@ -143,7 +141,6 @@ const changeCateList=(i)=>{
         }
 
         padding-right: 5px;
-
     }
 
     span {
@@ -152,15 +149,14 @@ const changeCateList=(i)=>{
         overflow: hidden;
         text-overflow: ellipsis;
         width: 100px;
-
     }
 }
 
 .cateItem:hover {
-    background: #F3F3F3;
+    background: #f3f3f3;
 }
 
 .active {
-    background: #F3F3F3;
+    background: #f3f3f3;
 }
 </style>
