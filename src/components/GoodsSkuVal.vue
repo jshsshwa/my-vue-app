1c1192a96bae50edf9e977299dde87c15aabd1cc
Sean <jshsshwa@gmail.com>
2024-05-22 10:24:06 +0800

    fix

--- src/components/GoodsSkuVal.vue
+++ src/components/GoodsSkuVal.vue
@@ -1,49 +1,38 @@
-<!-- 规格值
-     GoodsSkuAdd的子组件
+<!-- 規格值
+     GoodsSkuAdd的組組件
 -->
 <template>
     <div>
         <!-- <span v-for="(sub, i) in item.goodsSkusCardValue" :key="i">
                         {{ sub.text }}
                     </span> -->
-        <el-tag v-for="(tag, i) in item.goodsSkusCardValue" :key="i" closable :disable-transitions="false"
-            @close="handleClose(tag)">
+        <el-tag v-for="(tag, i) in item.goodsSkusCardValue" :key="i" closable :disable-transitions="false" @close="handleClose(tag)">
             <el-input v-model="tag.text" class="inputStyle" @change="editSkuVal($event, tag)"></el-input>
-
         </el-tag>
-        <el-input v-if="inputVisible" ref="InputRef" v-model="inputValue" class="inputStyle" size="small"
-            @keyup.enter="handleInputConfirm()" @blur="handleInputConfirm()" />
-        <el-button v-else class="button-new-tag ml-1" size="small" @click="showInput">
-            + New Tag
-        </el-button>
-
+        <el-input
+            v-if="inputVisible"
+            ref="InputRef"
+            v-model="inputValue"
+            class="inputStyle"
+            size="small"
+            @keyup.enter="handleInputConfirm()"
+            @blur="handleInputConfirm()"
+        />
+        <el-button v-else class="button-new-tag ml-1" size="small" @click="showInput"> + New Tag </el-button>
     </div>
 </template>
 
 <script setup>
-import { initSkuItemVal } from '@/utils/useSku.js'
+import {initSkuItemVal} from '@/utils/useSku.js';
 
-
-
-
-
-
-//接收父组件传递的id
+//接收父組件傳遞的id
 const props = defineProps({
     skuId: [Number, String]
-})
-const { item,
-    inputValue,
-    inputVisible,
-    InputRef,
-    handleClose,
-    showInput,
-    handleInputConfirm, 
-    editSkuVal } = initSkuItemVal(props.skuId)
-
+});
+const {item, inputValue, inputVisible, InputRef, handleClose, showInput, handleInputConfirm, editSkuVal} = initSkuItemVal(props.skuId);
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .el-tag {
     margin-right: 10px;
 }
@@ -52,4 +41,5 @@ const { item,
     width: 70px;
     height: 18px;
     font-size: 12px;
-}</style>
+}
+</style>
--- src/components/skuTable.vue
+++ src/components/skuTable.vue
@@ -1,25 +1,25 @@
-<!-- 多规格表格 -->
+<!-- 多規格表格 -->
 <template>
-    <el-form-item label="规格设置">
+    <el-form-item label="設格設置">
         <table cellpadding="0" cellspacing="0">
             <thead>
                 <tr>
-                    <th v-for="(item,i) in tableTitle" :key="i" :rowspan="item.row" :colspan="item.col">
+                    <th v-for="(item, i) in tableTitle" :key="i" :rowspan="item.row" :colspan="item.col">
                         {{ item.name }}
                     </th>
                 </tr>
                 <tr>
-                    <th v-for="(item,i) in isSkuVal" :key="i">
+                    <th v-for="(item, i) in isSkuVal" :key="i">
                         {{ item.name }}
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(item,i) in skuTable" :key="i">
-                    <td v-for="(sub,index) in item.skus" :key="index" width="60px">
-                        {{ sub.value }}                    
+                <tr v-for="(item, i) in skuTable" :key="i">
+                    <td v-for="(sub, index) in item.skus" :key="index" width="60px">
+                        {{ sub.value }}
                     </td>
-                   
+
                     <td>
                         <el-input v-model="item.oprice" type="number"></el-input>
                     </td>
@@ -39,33 +39,31 @@
                         <el-input v-model="item.weight" type="number"></el-input>
                     </td>
                     <td>
-                        <el-input v-model="item.code" ></el-input>
+                        <el-input v-model="item.code"></el-input>
                     </td>
                 </tr>
             </tbody>
         </table>
-
     </el-form-item>
 </template>
 
 <script setup>
-import { initTable } from '@/utils/useSku.js'
-const { isSkuVal, tableTitle, skuTable } = initTable()
+import {initTable} from '@/utils/useSku.js';
+const {isSkuVal, tableTitle, skuTable} = initTable();
 
-console.log(skuTable.value)
-
+console.log(skuTable.value);
 </script>
 
-<style lang='less' scoped>
- table{
+<style lang="less" scoped>
+table {
     border: 1px solid #dbdbdb;
     width: 100%;
-    thead tr th{
-        border: 1px solid #dbdbdb;        
+    thead tr th {
+        border: 1px solid #dbdbdb;
     }
-    tbody tr td{
+    tbody tr td {
         border: 1px solid #dbdbdb;
         text-align: center;
     }
- } 
+}
 </style>
